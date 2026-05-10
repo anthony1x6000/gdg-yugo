225c1e2adb3d881b43ed387d366f3fe1c7e92242
Anthony <anthony@anthonyis.online>
2026-05-10 05:22:54 +0000

    feat: better docs

--- backend/scraper/.wrangler/tmp/bundle-HCPnXX/middleware-loader.entry.ts
+++ backend/scraper/.wrangler/tmp/bundle-HCPnXX/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/workspaces/gdg-yugo/backend/scraper/.wrangler/tmp/bundle-cwA7Td/middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/workspaces/gdg-yugo/backend/scraper/.wrangler/tmp/bundle-HCPnXX/middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "/workspaces/gdg-yugo/node_modules/.pnpm/wrangler@4.90.0_@cloudflare+workers-types@4.20260510.1/node_modules/wrangler/templates/middleware/common.ts";
-import type { WorkerEntrypointConstructor } from "/workspaces/gdg-yugo/backend/scraper/.wrangler/tmp/bundle-cwA7Td/middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "/workspaces/gdg-yugo/backend/scraper/.wrangler/tmp/bundle-HCPnXX/middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "/workspaces/gdg-yugo/backend/scraper/.wrangler/tmp/bundle-cwA7Td/middleware-insertion-facade.js";
+export * from "/workspaces/gdg-yugo/backend/scraper/.wrangler/tmp/bundle-HCPnXX/middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
